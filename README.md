f34c28a1ba359d246dd27b3bb3f91857625f87c4
Dominic Sayers <dominic@sayers.cc>
2017-01-26 13:07:10 +0000

    Use aws instead of s3cmd

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # s3back
 Script to backup stuff to S3
+
+### Installation
+
+1.  `sudo apt-get install awscli`
+1.  `cd ~/apps`
+1.  `git clone git@github.com:dominicsayers/s3back.git`
+1.  `cd s3back`
+1.  `pico .awsrc` and add your credentials as below
+
+### .awsrc
+
+```
+AWS_SECRET_ACCESS_KEY=<your secret access key>
+AWS_ACCESS_KEY_ID=<your access key>
+AWS_DEFAULT_REGION=<your region>
+```
---
+++ distribute.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd "$(dirname $0)"
+
+BASE=$(pwd)
+
+set -a
+source .awsrc
+set +a
+
+mkdir -p backups
+chmod a+w backups
+cd backups
+
+for f in *
+do
+        EXTENSION=
+        ROOT=${f}
+
+        while [[ ${ROOT} = ?*.* ]] ; do
+                EXTENSION=${ROOT##*.}.${EXTENSION}
+                ROOT=${ROOT%.*}
+        done
+
+        EXTENSION=${EXTENSION%.*}
+
+        if [[ ${ROOT} != postgres ]] && [[ ${ROOT} != template1 ]];
+        then
+                BUCKET=$(echo ${ROOT} | sed -e s/_/-/)
+                echo Sending ${ROOT} to bucket ${BUCKET}
+                env
+                # s3cmd -c "${BASE}/.s3cfg" --rr -H put ${f} "s3://${BUCKET}/backups/${ROOT}-`date +%Y-%m-%d`.${EXTENSION}"
+                aws s3 cp ${f} "s3://${BUCKET}/backups/${ROOT}-`date +%Y-%m-%d`.${EXTENSION}"
+        fi
+done
